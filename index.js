1404aee96cdb4b2fa3572b76ef5f41dfc9236bc7
Claudio Gentile <claudgent@gmail.com>
2018-04-04 15:08:07 -0400

    added a dropdown menu for mobile

--- index.js
+++ index.js
@@ -9,4 +9,28 @@
         }, 1000, 'easeInOutCirc');
         event.preventDefault();
     });
+    // feature for dropdown TODO: COMBINE FUNCTIONS
+    $('.dropdown a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top)
+        }, 1000, 'easeInOutCirc');
+        event.preventDefault();
+    });
 })(jQuery);
+
+function dropDown(x) {
+    showMenuContent();
+    changeShape(x);
+}
+
+// start hamburger menu animation
+function changeShape(x) {
+    x.classList.toggle("change");
+}
+
+function showMenuContent() {
+    document.getElementById("dropdown-container")
+    .classList
+    .toggle("show");
+}
