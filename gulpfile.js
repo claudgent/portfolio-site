406cfcb784429b9d2994e990ca2d6e39f7d6611f
Claudio Gentile <claudgent@gmail.com>
2018-05-08 20:35:38 -0400

    use babel for es5+ js

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,30 @@ const imagemin = require('gulp-imagemin');
 const rename = require('gulp-rename');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
- 
+const babel = require('gulp-babel');
+
 gulp.task('css', () => {
-    gulp.src('css/*.css')
+  gulp.src('css/*.css')
     .pipe(gulp.dest('css'))
-    .pipe(cleanCSS({compatibility: 'ie10'}))
-    .pipe(rename({extname:'.min.css' }))
+    .pipe(cleanCSS({ compatibility: 'ie10' }))
+    .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('css'));
 });
 
 gulp.task('js', () => {
-    gulp.src('js/*.js')
+  gulp.src('js/*.js')
     .pipe(gulp.dest('js'))
+    .pipe(babel({
+      presets: ['env'],
+    }))
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
     .pipe(gulp.dest('js'));
 });
 
 gulp.task('images', () =>
-    gulp.src('img/*')
+  gulp.src('img/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('img'))
-);
+    .pipe(gulp.dest('img')));
 
-gulp.task('default', ['css','js','images'] );
+gulp.task('default', ['css', 'js', 'images']);
