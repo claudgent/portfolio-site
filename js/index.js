f44a2af5fd4d2c98c7eb978d8ba1df6e0760fce4
Claudio Gentile <claudgent@gmail.com>
2018-05-08 20:45:48 -0400

    scroll handler now applied to a 'scroll' class

--- js/index.js
+++ js/index.js
@@ -1,36 +1,28 @@
-(function($) {
-    "use strict";
-
-    // jQuery for page scrolling feature
-    $('.menu a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top)
-        }, 1000, 'easeInOutCirc');
-        event.preventDefault();
-    });
-    // feature for dropdown TODO: COMBINE FUNCTIONS
-    $('.dropdown a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top)
-        }, 1000, 'easeInOutCirc');
-        event.preventDefault();
-    });
-})(jQuery);
-
-function dropDown(x) {
-    showMenuContent();
-    changeShape(x);
+function pageScroll($) {
+  // jQuery for page scrolling feature
+  $('.scroll').on('click', (event) => {
+    const $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top),
+    }, 1000, 'easeInOutCirc');
+    event.preventDefault();
+  });
 }
 
 // start hamburger menu animation
 function changeShape(x) {
-    x.classList.toggle("change");
+  x.classList.toggle('change');
 }
 
 function showMenuContent() {
-    document.getElementById("dropdown-container")
+  document.getElementById('dropdown-container')
     .classList
-    .toggle("show");
+    .toggle('show');
 }
+
+function dropDown(x) {
+  showMenuContent();
+  changeShape(x);
+}
+
+pageScroll(jQuery);
