49cc7c2cdc1efb20bcec80127215f1a84b8d0e26
Claudio Gentile <claudgent@gmail.com>
2018-05-14 15:52:35 -0400

    Automatic css animations added after page load

--- js/index.js
+++ js/index.js
@@ -1,25 +1,32 @@
+$( () => {
+  // Event listener for page scrolling feature
 
-// jQuery for page scrolling feature
-$('.scroll').on('click', function (event) {
-  const $anchor = $(this);
-  $('html, body').stop().animate({
-    scrollTop: ($($anchor.attr('href')).offset().top),
-  }, 1000, 'easeInOutCirc');
-  event.preventDefault();
+  $('.scroll').on('click', function (event) {
+    const $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top),
+    }, 1000, 'easeInOutCirc');
+    event.preventDefault();
+  });
+
+  // Activate animations after page has loaded
+
+  $('#top-border').addClass('grow-border');
+  $('.animate').addClass('fadeIn');
+
+  // Add Animations for DropDown Menu
+  function changeShape(x) {
+    x.classList.toggle('change');
+  }
+
+  function showMenuContent() {
+    document.getElementById('dropdown-container')
+      .classList
+      .toggle('show');
+  }
+
+  function dropDown(x) {
+    showMenuContent();
+    changeShape(x);
+  }
 });
-
-// start hamburger menu animation
-function changeShape(x) {
-  x.classList.toggle('change');
-}
-
-function showMenuContent() {
-  document.getElementById('dropdown-container')
-    .classList
-    .toggle('show');
-}
-
-function dropDown(x) {
-  showMenuContent();
-  changeShape(x);
-}
